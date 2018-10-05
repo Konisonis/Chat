4b75e82beee740c38e584fd6466080933c49ce9f
Konstantin Rosenberg <konsi.rs@web.de>
2018-10-05 11:49:37 +0200

    Fixed Bugs and sone security issues. Tried to add file chooser(does't work already)

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ let app = express();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
 
+let fs = require('fs');//TODO
+
+
 let date = new Date();
 
 //contains Sockets for quick access with username
@@ -35,51 +38,74 @@ io.on('connection', (socket) => {
     socket.on('chat message', (message) => {
         let user = socket.user;
         let msg = message;
-        io.emit('chat message', user + ': ' + msg + '   |  ' + date.toUTCString()); // TODO: sends message to every connected Socket. So it sends them to users that are not even logged in.
+        if (socket.user && message) {
+            allUsernames.forEach((username) => {
+                let userSocket = connectedUsers[username];
+                if (userSocket) {
+                    userSocket.emit('chat message', user + ': ' + msg + '   |  ' + date.toUTCString()); // Only sends message to logged in users not to all
+
+                }
+            });
+        }
     });
 
     //new client log-in
     socket.on('login', (username, callback) => {
         //if username is already taken
-        if (connectedUsers[username]) {
-            callback(false);
-            //if username is accepted and login was successful
-        } else {
-            socket.user = username;
-            callback(true);
-            connectedUsers[username] = socket;
-            allUsernames.push(username);
-            io.emit('chat message', 'User connected: ' + username);
-            io.emit('user list', allUsernames);
+        try {
+            if (connectedUsers[username] || socket.user || !username) {
+                callback(false);
+                //if username is accepted and login was successful
+            } else {
+                socket.user = username;
+                callback(true);
+                connectedUsers[username] = socket;
+                allUsernames.push(username);
+                io.emit('chat message', 'User connected: ' + username);
+                io.emit('user list', allUsernames);
+            }
+        }
+        catch (err) {
+            console.log(err);
         }
 
     });
 
     //client demands list of connected users
-    socket.on('user list',(callback)=>{
+    socket.on('user list', (callback) => {
         callback(allUsernames);
     });
 
     //Send message only to one specific client
-    socket.on('private message', (data) => {
+    socket.on('private message', (msg, receiver) => {
         //connectedUsers contains the sockets from each logged in user
-        let receiverSocket = connectedUsers[data.receiver];
+        let receiverSocket = connectedUsers[receiver];
         let user = socket.user;
-        let msg = socket.user;
-        if(receiverSocket){
-            receiverSocket.emit('chat message', 'PRIVATE MESSAGE '+user + ': ' + msg + '   |  ' + date.toUTCString());
+        if (receiverSocket && msg) {
+            receiverSocket.emit('chat message', 'PRIVATE MESSAGE ' + user + ': ' + msg + '   |  ' + date.toUTCString());
         }
+    });
+
+    socket.on('file', (file, callback) => {
+        callback();
+        io.emit('file', file);
+        fs.writeFile("mandala.png", file, (err) => {
+            if (err) {
+                throw err;
+            }
+            console.log("The file was saved!");
+        });
     });
 
 });
 
 function removeUser(socket) {
     //remove username from allUsernames array
-    let index = allUsernames.findIndex((username)=>{
+    let index = allUsernames.findIndex((username) => {
         return username === socket.user;
     });
-    if(index > -1){
-        allUsernames.splice(index,1);
+    if (index > -1) {
+        allUsernames.splice(index, 1);
     }
 
     //remove Socket from connectedUsers JSON
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,7 @@
 $(() => {
     var socket = io();
+
+    //Log in with user name
     $('#loginForm').submit(() => {
             socket.emit('login', $('#user').val(), (ok) => {
                 if (ok) {
@@ -15,22 +17,39 @@ $(() => {
         }
     );
 
-    //send a chat message to everyone
+    //send a chat message to everyone in the chat
     $('#chatForm').submit(() => {
         socket.emit('chat message', $('#m').val());
         $('#m').val('');
         return false;
     });
 
+    //send a file
+    $('#sendFile').click(()=>{
+        let file = $('#inputFile').val();
+        if(file){
+            socket.emit('file', file,()=>{
+                console.log('file successfully uploaded');
+            });
+        }
+    });
+
+    //Receiving a message
     socket.on('chat message', (msg) => {
         $('#messages').append($('<li>').text(msg));
     });
 
+    //Receiving an updated user list
     socket.on('user list', (userArray) => {
         $('#users').empty();
         userArray.forEach((user) => {
             $('#users').append($('<li>').text(user));
         });
+    });
+
+    //Receiving a file
+    socket.on('file',(file)=>{
+
     });
 
 });
